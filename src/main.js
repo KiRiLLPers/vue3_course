8ab5e1653a76a8dfe99cdd7e789e5ac47af87eb6
Kirill <dorc99@mail.ru>
2022-10-23 17:09:34 +0700

    delete console.log

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
 // import router from './router';
 // import store from './store';
-console.log();
-createApp(App).mount('#app');
+createApp(App).mount("#app");
